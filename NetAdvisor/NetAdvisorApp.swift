7f8e220611d46ad8131da4c6441756807f9dbfc4
konda rama krishna <kondaramakrishna99@gmail.com>
2025-12-14 18:19:35 +0530

    Initial Commit

---
+++ NetAdvisor/NetAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetAdvisorApp.swift
+//  NetAdvisor
+//
+//  Created by Rama Krishna Konda on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NetAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
